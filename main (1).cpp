072648062dde02938378aeeac3b46db9447c60c1
ddezue <LYANAZAYKA@MAIL.RU>
2025-10-16 18:27:37 +0700

    Update lab05 2.0.cpp

--- main (1).cpp
+++ main (1).cpp
@@ -1,49 +1,57 @@
 #include <stdio.h>
 #include <math.h>
 
-int sumOfAbsoluteValues(const int* array, int length) {
-    int sum, arraySize;
-    for (int elementIndex = 0; elementIndex < arraySize; ++elementIndex) {
-        sum += abs(array[elementIndex]);
-    }
-    return sum;
+int sumOfAbsoluteValues(const int* array, int arraySize) {
+  double sum = 0;
+  int elementIndex;
+  
+  for (elementIndex = 0; elementIndex < arraySize; ++elementIndex) {
+    sum += abs(array[elementIndex]);
+  }
+  
+  return sum;
 }
 
 int main() {
-    int sizeOfTheArrayX, sizeOfTheArrayY, sizeOfTheArrayZ;
-    double sumX, sumY, sumZ;
-    int arrayX[999], arrayY[999], arrayZ[999];
-    int elementIndex;
+  int sizeOfTheArrayX, sizeOfTheArrayY, sizeOfTheArrayZ;
+  int sumX, sumY, sumZ;
+  int arrayX[999], arrayY[999], arrayZ[999];
+  int elementIndex;
     
-    printf("Enter the size of the array X: ");
-    scanf("%d", &sizeOfTheArrayX);
+  printf("Enter the size of the array X: ");
+  scanf("%d", &sizeOfTheArrayX);
     
-    printf("Enter the elements of the X array: \n");
-    for (elementIndex = 0; elementIndex < sizeOfTheArrayX; ++elementIndex) {
+  printf("Enter the elements of the X array: \n");
+  for (elementIndex = 0; elementIndex < sizeOfTheArrayX; ++elementIndex) {
     scanf("%d", &arrayX[elementIndex]);
   }
-
-    printf("Enter the size of the array Y: ");
-    scanf("%d", &sizeOfTheArrayY);
+  
+  printf("\n");
+  printf("Enter the size of the array Y: ");
+  scanf("%d", &sizeOfTheArrayY);
     
-    printf("Enter the elements of the Y array: \n");
-    for (elementIndex = 0; elementIndex < sizeOfTheArrayY; ++elementIndex) {
+  printf("Enter the elements of the Y array: \n");
+  for (elementIndex = 0; elementIndex < sizeOfTheArrayY; ++elementIndex) {
     scanf("%d", &arrayY[elementIndex]);
   }
 
-    printf("Enter the size of the array Z: ");
-    scanf("%d", &sizeOfTheArrayZ);
+  printf("\n");
+  printf("Enter the size of the array Z: ");
+  scanf("%d", &sizeOfTheArrayZ);
     
-    printf("Enter the elements of the Z array: \n");
-    for (elementIndex = 0; elementIndex < sizeOfTheArrayZ; ++elementIndex) {
+  printf("Enter the elements of the Z array: \n");
+  for (elementIndex = 0; elementIndex < sizeOfTheArrayZ; ++elementIndex) {
     scanf("%d", &arrayZ[elementIndex]);
   }
+  
+  printf("\n");
+  sumX = sumOfAbsoluteValues(arrayX, sizeOfTheArrayX);
+  sumY = sumOfAbsoluteValues(arrayY, sizeOfTheArrayY);
+  sumZ = sumOfAbsoluteValues(arrayZ, sizeOfTheArrayZ);
 
-
-    printf("\nРезультаты:\n");
-    printf("Сумма модулей элементов массива X: %d\n", sumX);
-    printf("Сумма модулей элементов массива Y: %d\n", sumY);
-    printf("Сумма модулей элементов массива Z: %d\n", sumZ);
+  printf("Sum of modules of the array elements X: %d\n", sumX);
+  printf("Sum of modules of the array elements Y: %d\n", sumY);
+  printf("Sum of modules of the array elements Z: %d\n", sumZ);
     
-    return 0;
+  return 0;
 }
